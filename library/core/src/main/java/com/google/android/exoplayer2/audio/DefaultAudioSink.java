77cca337be6decb426f749f6392b827e68706945
Bryan Ischo <bryan@ischo.com>
2019-06-05 13:53:19 -0700

    Ignore DEAD_OBJECT errors in AudioSink; this allows playback to proceed without error through HDMI hotplug removal/insertion.

--- library/core/src/main/java/com/google/android/exoplayer2/audio/DefaultAudioSink.java
+++ library/core/src/main/java/com/google/android/exoplayer2/audio/DefaultAudioSink.java
@@ -767,7 +767,16 @@ public final class DefaultAudioSink implements AudioSink {
     lastFeedElapsedRealtimeMs = SystemClock.elapsedRealtime();
 
     if (bytesWritten < 0) {
-      throw new WriteException(bytesWritten);
+        if (bytesWritten == AudioTrack.ERROR_DEAD_OBJECT) {
+            // The AudioTrack being dead is something that presumably a higher
+            // level will deal with.  Here we just pretend like all the audio
+            // was played, in effect making audio played to a dead object just
+            // do nothing.
+            bytesWritten = bytesRemaining;
+        }
+        else {
+            throw new WriteException(bytesWritten);
+        }
     }
 
     if (isInputPcm) {
