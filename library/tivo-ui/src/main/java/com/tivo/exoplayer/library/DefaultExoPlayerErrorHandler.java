488f5a44b425891c0b0a6ab3be5a39ffd274babb
Steve Mayhew <mayhew.steve@gmail.com>
2019-10-16 20:22:47 -0700

    Pull defaults to DefaultExoPlayerErrorHandler
    This allows extending it without `AbstractMethodErrors` with the lime tools (hopefully)

--- library/tivo-ui/src/main/java/com/tivo/exoplayer/library/DefaultExoPlayerErrorHandler.java
+++ library/tivo-ui/src/main/java/com/tivo/exoplayer/library/DefaultExoPlayerErrorHandler.java
@@ -1,10 +1,19 @@
 package com.tivo.exoplayer.library;
 
+import android.view.Surface;
 import androidx.annotation.CallSuper;
+import androidx.annotation.Nullable;
 import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.analytics.AnalyticsListener;
+import com.google.android.exoplayer2.audio.AudioAttributes;
+import com.google.android.exoplayer2.decoder.DecoderCounters;
+import com.google.android.exoplayer2.metadata.Metadata;
 import com.google.android.exoplayer2.source.MediaSourceEventListener;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.util.Log;
 import java.io.IOException;
 import java.util.List;
@@ -88,4 +97,214 @@ public class DefaultExoPlayerErrorHandler implements AnalyticsListener {
       boolean wasCanceled) {
     Log.d(TAG, "onLoadError - URL: " + loadEventInfo.uri + " io error: "+error);
   }
+
+  // Implement balance of AnalyticsListener to keep lime tools happy
+
+
+  @Override
+  public void onPlayerStateChanged(EventTime eventTime, boolean playWhenReady, int playbackState) {
+
+  }
+
+  @Override
+  public void onTimelineChanged(EventTime eventTime, int reason) {
+
+  }
+
+  @Override
+  public void onPositionDiscontinuity(EventTime eventTime, int reason) {
+
+  }
+
+  @Override
+  public void onSeekStarted(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onSeekProcessed(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onPlaybackParametersChanged(EventTime eventTime,
+      PlaybackParameters playbackParameters) {
+
+  }
+
+  @Override
+  public void onRepeatModeChanged(EventTime eventTime, int repeatMode) {
+
+  }
+
+  @Override
+  public void onShuffleModeChanged(EventTime eventTime, boolean shuffleModeEnabled) {
+
+  }
+
+  @Override
+  public void onLoadingChanged(EventTime eventTime, boolean isLoading) {
+
+  }
+
+  @Override
+  public void onTracksChanged(EventTime eventTime, TrackGroupArray trackGroups,
+      TrackSelectionArray trackSelections) {
+
+  }
+
+  @Override
+  public void onLoadStarted(EventTime eventTime,
+      MediaSourceEventListener.LoadEventInfo loadEventInfo,
+      MediaSourceEventListener.MediaLoadData mediaLoadData) {
+
+  }
+
+  @Override
+  public void onLoadCompleted(EventTime eventTime,
+      MediaSourceEventListener.LoadEventInfo loadEventInfo,
+      MediaSourceEventListener.MediaLoadData mediaLoadData) {
+
+  }
+
+  @Override
+  public void onLoadCanceled(EventTime eventTime,
+      MediaSourceEventListener.LoadEventInfo loadEventInfo,
+      MediaSourceEventListener.MediaLoadData mediaLoadData) {
+
+  }
+
+  @Override
+  public void onDownstreamFormatChanged(EventTime eventTime,
+      MediaSourceEventListener.MediaLoadData mediaLoadData) {
+
+  }
+
+  @Override
+  public void onUpstreamDiscarded(EventTime eventTime,
+      MediaSourceEventListener.MediaLoadData mediaLoadData) {
+
+  }
+
+  @Override
+  public void onMediaPeriodCreated(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onMediaPeriodReleased(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onReadingStarted(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onBandwidthEstimate(EventTime eventTime, int totalLoadTimeMs, long totalBytesLoaded,
+      long bitrateEstimate) {
+
+  }
+
+  @Override
+  public void onSurfaceSizeChanged(EventTime eventTime, int width, int height) {
+
+  }
+
+  @Override
+  public void onMetadata(EventTime eventTime, Metadata metadata) {
+
+  }
+
+  @Override
+  public void onDecoderEnabled(EventTime eventTime, int trackType,
+      DecoderCounters decoderCounters) {
+
+  }
+
+  @Override
+  public void onDecoderInitialized(EventTime eventTime, int trackType, String decoderName,
+      long initializationDurationMs) {
+
+  }
+
+  @Override
+  public void onDecoderInputFormatChanged(EventTime eventTime, int trackType, Format format) {
+
+  }
+
+  @Override
+  public void onDecoderDisabled(EventTime eventTime, int trackType,
+      DecoderCounters decoderCounters) {
+
+  }
+
+  @Override
+  public void onAudioSessionId(EventTime eventTime, int audioSessionId) {
+
+  }
+
+  @Override
+  public void onAudioAttributesChanged(EventTime eventTime, AudioAttributes audioAttributes) {
+
+  }
+
+  @Override
+  public void onVolumeChanged(EventTime eventTime, float volume) {
+
+  }
+
+  @Override
+  public void onAudioUnderrun(EventTime eventTime, int bufferSize, long bufferSizeMs,
+      long elapsedSinceLastFeedMs) {
+
+  }
+
+  @Override
+  public void onDroppedVideoFrames(EventTime eventTime, int droppedFrames, long elapsedMs) {
+
+  }
+
+  @Override
+  public void onVideoSizeChanged(EventTime eventTime, int width, int height,
+      int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+
+  }
+
+  @Override
+  public void onRenderedFirstFrame(EventTime eventTime, @Nullable Surface surface) {
+
+  }
+
+  @Override
+  public void onDrmSessionAcquired(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onDrmKeysLoaded(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onDrmSessionManagerError(EventTime eventTime, Exception error) {
+
+  }
+
+  @Override
+  public void onDrmKeysRestored(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onDrmKeysRemoved(EventTime eventTime) {
+
+  }
+
+  @Override
+  public void onDrmSessionReleased(EventTime eventTime) {
+
+  }
 }
+
